d31421bc0066b842d4e895fe8e712fe66085ac79
Doug Turnbull <softwaredoug@gmail.com>
2015-02-09 14:59:36 -0500

    Start elasticsearch on first creating box

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,7 @@ sudo add-apt-repository "deb http://packages.elasticsearch.org/elasticsearch/1.4
 sudo apt-get update
 sudo apt-get install elasticsearch
 sudo update-rc.d elasticsearch defaults 95 10
+sudo /etc/init.d/elasticsearch start
 
 # Install Elasticsearch plugins
 cd /usr/share/elasticsearch
